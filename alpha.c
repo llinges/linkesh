e63e75beac4124fc795c7bcb956e3645939675c9
llinges <35653011+llinges@users.noreply.github.com>
2018-01-30 16:03:29 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch();
+clrscr();
+printf("enter a character");
+scanf("%c",&ch);
+if(ch>'a'&&ch<'z');
+{
+printf("alphabet");
+}
+else
+{
+printf("not an alphabet");
+}
+getch();
+}
