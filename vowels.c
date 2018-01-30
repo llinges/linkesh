4b4eba76e4c5bf380a61dba42f02786719f144fe
llinges <35653011+llinges@users.noreply.github.com>
2018-01-30 16:35:49 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("enter the letter:",ch);
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+{
+printf("voewl");
+}
+else
+{
+printf("constant");
+}
+getch();
+}
