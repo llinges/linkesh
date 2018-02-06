dc6c0fbeda59a5be92e6708d8917db529739226b
llinges <35653011+llinges@users.noreply.github.com>
2018-02-06 16:48:51 +0530

    Create display.c

---
+++ display.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter the a,b value");
+scanf("%d %d&a&b");
+if(a>b)
+{
+printf("\n a is even number ");
+else
+printf(" a is odd number ");
+printf("even number between two intervals");
+getch();
+}
