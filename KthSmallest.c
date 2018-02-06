5fd1b9ba31fe8ab9fbfbf0bc7884466bf9336a4f
llinges <35653011+llinges@users.noreply.github.com>
2018-02-06 16:32:51 +0530

    Create KthSmallest.c

---
+++ KthSmallest.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<onio.h>
+void main()
+{
+int n,k,a[10],i;
+clrscr();
+printf("N&k value:");
+scanf("%d %d",&n,&k);
+printf("\n");
+printf("Enter the array:");
+for(i=1;i<=n;i++)
+{
+scanf("\t%d",&a[i]);
+}
+for(i=1;i<n;i++)
+{
+int temp;
+for(int j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d",a[k]);  
+getch();
+}
