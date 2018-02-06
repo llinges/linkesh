bf954f57bfa5eb80257a8c780dd71601acab5136
llinges <35653011+llinges@users.noreply.github.com>
2018-02-06 16:07:58 +0530

    Rename integer.c to Integer.c

---
+++ Integer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int a;
+int count = 0;
+clrscr();
+printf("Enter an integer: ");
+scanf("%d", &a);
+while(a != 0)
+{
+a = 10;
+++count;
+}
+printf("Number of digits: %d", count);
+getch();
+return 0;
+}
