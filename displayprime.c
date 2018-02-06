2a75ebbe05be5e455c34be90e1677f3c8daa1b11
llinges <35653011+llinges@users.noreply.github.com>
2018-02-06 16:52:11 +0530

    Create displayprime.c

---
+++ displayprime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int i,n,f=0;
+clrscr();
+printf("Get The Any Poisitive Integer:\n");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+f=1;
+break;
+}       
+}
+if(f==0)
+{
+printf("If it's a Prime Number:\n");
+}
+else
+{
+printf("If It's a Non Prime Number\n");
+getch();
+}
+}
