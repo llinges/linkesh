2cba9d0cc04f3a9f42b4da5ee7a49cbf68dfd8a1
llinges <35653011+llinges@users.noreply.github.com>
2018-02-08 16:41:05 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include,conio.h>
+void main()
+{
+int a[3];
+clrscr();
+printf("enter the value of a");
+scanf("%d",&n);
+printf("enter the second value of a");
+scanf("%d",&s);
+int a++;
+printf("%d",&n);
+getch();
+}
