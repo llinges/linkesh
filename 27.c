9d76d0b365d9f533c50fb215db8c3e34e196092d
llinges <35653011+llinges@users.noreply.github.com>
2018-02-13 15:48:45 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter number");
+scanf("%d",&n);
+if(isdigit(n));
+{
+printf("yes");
+}
+else
+{
+printf("no");
+getch();
+}
+}
