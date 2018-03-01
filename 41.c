62f645495a66b987e5a29885e4bca3970583f6b0
llinges <35653011+llinges@users.noreply.github.com>
2018-03-01 16:03:58 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+void main() 
+{
+char a[100];
+clrscr();
+int i=1,n;
+printf("Enter the string:\t");
+gets(a);
+printf("Enter the number:\n");
+scanf("%d\n",&n);
+while(i<=n)
+{
+printf("%s\n",a);	
+i++;	
+getch();
+}
+}
