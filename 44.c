ed0a089ddaf80981fd0f7b45d45dd488f3f15aa3
llinges <35653011+llinges@users.noreply.github.com>
2018-03-02 14:12:30 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n>0&&n<10)
+printf("yes");
+else
+printf("\n no");
+getch();
+}
