c9f69149b707590b9c0978d49f1cefbc86ce265e
llinges <35653011+llinges@users.noreply.github.com>
2018-03-03 14:36:45 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2;
+clrscr();
+scanf("%d %d",&n1,&n2);
+printf("\n the numbers are %d %d",n1,n2);
+int result;
+result=n1+n2;
+printf("\n  sum of the numbers are %d",result);
+getch();
+}
